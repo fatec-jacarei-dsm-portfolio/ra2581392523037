fa001958ee1cf87630ddad16cc63bb5f32528de7
Victor Ramos <v.ramos58@hotmail.com>
2026-05-12 21:35:47 -0300

    feat: arquivo mudando origem

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/docker",
+  assetPrefix: "/docker/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
