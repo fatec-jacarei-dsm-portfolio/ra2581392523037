262966aca40f120dede45476e18cb871ce0253f2
Victor Ramos <v.ramos58@hotmail.com>
2026-05-12 21:41:38 -0300

    fix: configuracaoes do caminho do projeto

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/ra2581392523037",
+  assetPrefix: "/ra2581392523037/",
   trailingSlash: true,
   images: {
     unoptimized: true,
